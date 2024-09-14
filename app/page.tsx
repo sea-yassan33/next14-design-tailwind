4d5bae5d3d8d19246044780527d597f4cb4f95f8
sea-yassan33 <a.yasuda_tr51@me.com>
2024-09-14 18:10:02 +0900

    update240914

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
 import Colllection from "./_components/Collection";
+import { tv } from 'tailwind-variants';
 
-const twStayles = {
-  //main
-  mainItem01:"p-4 md:w-1/3",
-  mainItem02:"h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden",
-  mainItemImg:"lg:h-48 md:h-36 w-full object-cover object-center",
-  mainItemTxtH1:"title-font text-lg font-medium text-gray-900 mb-3",
-  mainItemTxtH2:"tracking-widest text-xs title-font font-medium text-gray-400 mb-1",
-  mainItemTxt:"leading-relaxed mb-3",
-  mainTitle:"text-5xl font-bold tracking-widest bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent",
-}
+const twStayles = tv({
+  base:'font-bold tracking-widest bg-clip-text text-transparent mb-5',
+  variants: {
+    color:{
+      s01:'bg-gradient-to-r from-pink-500 to-violet-500',
+      s02:'bg-gradient-to-r from-yellow-500 to-green-500',
+    },
+    size:{
+      s01:'text-5xl',
+      s02:'text-4xl',
+    },
+  },
+});
 
 export default function Home() {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-10 mx-auto">
-        <h1 className={`${twStayles.mainTitle} mb-5`}>テンプレート集</h1>
+        <h1 className={twStayles({color:'s01',size:'s02'})}>テンプレート集</h1>
         <div className="flex flex-wrap -m-4">
           {/* テンプレート集 */}
           <Colllection/>
